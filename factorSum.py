f652691d5448a0dc3d492767b5042c3f927af335
DINH HUU PHAT <220048xn@yse-c.net>
2022-07-07 10:38:00 +0900

    FactorySum

---
+++ factorSum.py
@@ -0,0 +1,14 @@
+﻿def solve(n):
+    k=2
+    sum=0
+    while n>1:
+        while n%k==0:
+            sum+=k
+            n/=k
+        k+=1
+    return sum
+
+def factorSum(n):
+    while n!=solve(n):
+        n=solve(n);
+    return n
